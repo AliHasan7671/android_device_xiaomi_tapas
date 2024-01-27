eb37c15053a3aac9b45731af5202dd99950ecf5f
Ali Hasan <ahb7671@gmail.com>
2024-01-27 13:59:31 +0500

    tapas: rework init
    also set fingerpritns and descriptions for both variants
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- init/init_tapas.cpp
+++ init/init_tapas.cpp
@@ -42,48 +42,65 @@ std::vector<std::string> ro_props_default_source_order = {
     "vendor_dlkm.",
 };
 
-void property_override(string prop, string value)
-{
+void property_override(string prop, string value, bool add = true) {
     auto pi = (prop_info*) __system_property_find(prop.c_str());
 
     if (pi != nullptr)
         __system_property_update(pi, value.c_str(), value.size());
-    else
+    else if (add)
         __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
 }
 
-void load_redmi_topaz() {
-    property_override("bluetooth.device.default_name", "Redmi Note 12");
-    property_override("ro.product.brand", "Redmi");
-    property_override("ro.product.device", "topaz");
-    property_override("ro.product.manufacturer", "Xiaomi");
-    property_override("ro.product.marketname", "Redmi Note 12");
-    property_override("ro.product.model", "23021RAA2Y");
-    property_override("ro.product.mod_device", "topaz_global");
-    property_override("ro.product.name", "topaz_global");
-    property_override("vendor.usb.product_string", "Redmi Note 12");
+void set_ro_build_prop(const std::string &prop, const std::string &value) {
+    for (const auto &source : ro_props_default_source_order) {
+        auto prop_name = "ro." + source + "build." + prop;
+        if (source == "")
+            property_override(prop_name.c_str(), value.c_str());
+        else
+            property_override(prop_name.c_str(), value.c_str(), false);
+    }
 }
 
-void load_redmi_tapas() {
+void load_redmi_properties(string hwname) {
+    string device_name;
+    string model;
+    string mod_device;
+    string fingerprint;
+    string description;
+
+    if (hwname == "tapas") {
+	    device_name = "tapas";
+        model = "23021RAAEG";
+        mod_device = "tapas_global";
+        fingerprint = "Redmi/tapas_global/tapas:13/TKQ1.221114.001/V14.0.14.0.TMTMIXM:user/release-keys";
+        description = "tapas_global-user 13 TKQ1.221114.001 V816.0.2.0.UMTMIXM release-keys";
+    } else if (hwname == "topaz") {
+        device_name = "topaz";
+        model = "23021RAA2Y";
+        mod_device = "topaz_global";
+        fingerprint = "Redmi/tapas_global/tapas:13/TKQ1.221114.001/V14.0.14.0.TMTMIXM:user/release-keys";
+        description = "tapas_global-user 13 TKQ1.221114.001 V14.0.14.0.TMTMIXM release-keys";
+    }
+
+    // Set additional properties
+    set_ro_build_prop("fingerprint", fingerprint);
+    set_ro_build_prop("description", description);
+
     property_override("bluetooth.device.default_name", "Redmi Note 12");
     property_override("ro.product.brand", "Redmi");
-    property_override("ro.product.device", "tapas");
+    property_override("ro.product.device", device_name);
     property_override("ro.product.manufacturer", "Xiaomi");
     property_override("ro.product.marketname", "Redmi Note 12");
-    property_override("ro.product.model", "23021RAAEG");
-    property_override("ro.product.mod_device", "tapas_global");
-    property_override("ro.product.name", "tapas_global");
+    property_override("ro.product.model", model);
+    property_override("ro.product.mod_device", mod_device);
+    property_override("ro.product.name", mod_device);
     property_override("vendor.usb.product_string", "Redmi Note 12");
 }
 
 void vendor_load_properties() {
     std::string hwname = GetProperty("ro.boot.hwname", "");
     if (access("/system/bin/recovery", F_OK) != 0) {
-    if (hwname == "topaz") {
-        load_redmi_topaz();
-    } else if (hwname == "tapas") {
-        load_redmi_tapas();
-       }
+        load_redmi_properties(hwname);
     }
 
     // Set hardware revision
@@ -91,13 +108,12 @@ void vendor_load_properties() {
 
     // Set dalvik heap configuration
     std::string heapstartsize, heapgrowthlimit, heapsize, heapminfree,
-			heapmaxfree, heaptargetutilization;
+        heapmaxfree, heaptargetutilization;
 
     struct sysinfo sys;
     sysinfo(&sys);
 
     if (sys.totalram > 5072ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
         heapstartsize = "16m";
         heapgrowthlimit = "256m";
         heapsize = "512m";
@@ -105,7 +121,6 @@ void vendor_load_properties() {
         heapminfree = "8m";
         heapmaxfree = "32m";
     } else if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - phone-xhdpi-4096-dalvik-heap.mk
         heapstartsize = "8m";
         heapgrowthlimit = "192m";
         heapsize = "512m";
@@ -113,7 +128,6 @@ void vendor_load_properties() {
         heapminfree = "8m";
         heapmaxfree = "16m";
     } else {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
         heapstartsize = "16m";
         heapgrowthlimit = "256m";
         heapsize = "512m";
@@ -129,3 +143,4 @@ void vendor_load_properties() {
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
+
