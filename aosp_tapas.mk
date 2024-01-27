b90e2518c1c993a5f626f5397225864b4fe383d3
Ali Hasan <ahb7671@gmail.com>
2024-01-27 14:03:24 +0500

    tapas: set build description and fingerprint at compile time
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- aosp_tapas.mk
+++ aosp_tapas.mk
@@ -21,3 +21,8 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 12 4G
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="tapas_global-user 13 TKQ1.221114.001 V816.0.2.0.UMTMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/tapas_global/tapas:13/TKQ1.221114.001/V816.0.2.0.UMTMIXM:user/release-keys
