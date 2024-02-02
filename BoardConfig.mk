d0e564e8cdb66b008263850f85b31ff4120588ac
Luofan Chen <clfbbn@gmail.com>
2024-02-02 20:49:44 +0800

    tapas: Import QCC system_ext blobs
    Makes the QTI GNSS service happy. Thanks luca020400 for the help.
    
    Change-Id: I6de25b0b3fde6cce3a6cdf95f31c68734d429956
    Signed-off-by: Nikita Savchenko <NikitaSavc@yandex.ru>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -236,6 +236,7 @@ BOARD_AVB_ODM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     vendor/feather/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml \
     $(DEVICE_PATH)/configs/vintf/network_manifest.xml
