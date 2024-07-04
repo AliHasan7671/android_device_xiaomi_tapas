a77da4ce28508f2aeea1e1b84224d0a510c6a906
chickendrop89 <chickendrop89@gmail.com>
2024-07-04 21:13:43 +0200

    tapas: enable lazy RCU by default
    * Lazy RCU optimizes the RCU callbacks for power efficiency by delaying them on idle systems, and batching them for reduced CPU wakeups.
    * This improves battery life on phones and other power-sensitive devices.
    
    * ACK 5.15.148 has backported lazy RCU, and introduced a boot parameter to control this feature [1]
    * It is however disabled by default.
    
    * So, enable Lazy RCU by default using the "rcutree.enable_rcu_lazy" & "rcu_nocbs" BOARD_KERNEL_CMDLINE parameter
    
    [1] https://android.googlesource.com/kernel/common/+/b657edec328aa29f0ee3f6f7dda328ff3a2cb6b1
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,7 +100,9 @@ BOARD_BOOTCONFIG := \
     androidboot.usbcontroller=4e00000.dwc3
 
 BOARD_KERNEL_CMDLINE := \
-    video=vfb:640x400,bpp=32,memsize=3072000
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    rcu_nocbs=all \
+    rcutree.enable_rcu_lazy=1 \
 
 # Kernel prebuilt
 TARGET_KERNEL_ARCH := arm64
