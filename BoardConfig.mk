88836513669594272fa780dc7a93f9144ec783c3
@ZVNexus <akoskovich@pm.me>
2024-07-22 00:16:52 +0530

    tapas: Remove virtual framebuffer  * It's largely for testing purposes and it consumes    a large amount of memory. If your hardware doesn't    have graphics then it can be useful, which isn't the    case for shipped devices.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,6 @@ BOARD_BOOTCONFIG := \
     androidboot.usbcontroller=4e00000.dwc3
 
 BOARD_KERNEL_CMDLINE := \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
     rcu_nocbs=all \
     rcutree.enable_rcu_lazy=1 \
 
