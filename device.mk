76c330ced47c932de6c06d289916bc363ad066ec
AdarshGrewal <adarshgrewal@gmail.com>
2024-08-07 01:00:02 +0530

    tapas: Build libbatterylistener from source
    Change-Id: I8d3171869764e50544a5fcd7e793589e2ecbabc9
    Signed-off-by: Nikita Savchenko <NikitaSavc@yandex.ru>

--- device.mk
+++ device.mk
@@ -50,7 +50,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.primary.default \
     audio.r_submix.default \
-    audio.usb.default
+    audio.usb.default \
+    libbatterylistener
 
 PRODUCT_PACKAGES += \
     audioadsprpcd \
