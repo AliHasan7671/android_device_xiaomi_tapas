26dc81a4bcfd4aa0265a8dbf523980028df36689
Ali Hasan <ahb7671@gmail.com>
2024-10-26 15:39:59 +0500

    tapas: add lineage health
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- device.mk
+++ device.mk
@@ -321,6 +321,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
