a3c721e6de905f8d60cab4e9bdf1b38559c8ac97
Ali Hasan <ahb7671@gmail.com>
2025-03-27 00:41:06 +0500

    tapas: update to Redmi/tapas_global/tapas:13/TKQ1.221114.001/OS2.0.9.0.VMTMIXM:user/release-keys
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- aosp_tapas.mk
+++ aosp_tapas.mk
@@ -24,4 +24,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 #PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tapas_global-user 13 TKQ1.221114.001 V816.0.4.0.UMTMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/tapas_global/tapas:13/TKQ1.221114.001/V816.0.4.0.UMTMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/tapas_global/tapas:13/TKQ1.221114.001/OS2.0.9.0.VMTMIXM:user/release-keys
